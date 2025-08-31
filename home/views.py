744ecb6ae827c22b4ae6cce08029a30d9693f722
rawbeen10 <rdhakal861@gmail.com>
2025-08-31 20:32:33 +0545

    user authentication

--- home/views.py
+++ home/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
+
+
+# password: Rabin123@@
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render(request, 'index.html')
 
-def login(request):
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # check if user has entered correct details
+        user = authenticate(username=username, password=password)
+       
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect('/')
+        else:
+            # No backend authenticated the credentials
+            return render(request, 'login.html', {'error': 'Invalid email or password'})
+        
     return render(request, 'login.html')
 
-def logout(request):
-    return render(request, 'logout.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/login')
